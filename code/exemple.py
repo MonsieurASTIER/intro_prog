d9553cb04ec2d2046872d0a4c3c005455450a78e
skynet <me@mail.fr>
2023-06-23 18:09:52 +0200

    function test

--- code/exemple.py
+++ code/exemple.py
@@ -1,6 +1,6 @@
 #C'est ici que nous ecrivons nos programme!
 #Ecrivez simplement l'instruction suivante: print("hello world")
-def function_test:
+def function_test():
     print("hello world")
     print("a")
     return 10
