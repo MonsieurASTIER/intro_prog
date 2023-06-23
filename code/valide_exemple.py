efd59169ec79a04baa061d96c5f1d2b9c90d8231
skynet <me@mail.fr>
2023-06-23 18:20:51 +0200

    push

--- code/valide_exemple.py
+++ code/valide_exemple.py
@@ -2,16 +2,19 @@ import io
 import sys
 from exemple import function_test
 
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
 def test_Verif():
     try:
         retour = function_test()
         assert retour == 10," assert verifier"
         print("TECHIO> success true")
-        print("TECHIO> message bien jouer!!")
+        send_msg("victoire","bien jouer ")
 
     except AssertionError as e:
         print("TECHIO> success false")
-        print("TECHIO> message recommence avec 10")
+        send_msg("Echecs","recommence")
 
 if __name__ == "__main__":
     test_Verif()
