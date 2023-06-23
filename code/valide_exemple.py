52d1e9330df4f80a40532507920f94e435427918
skynet <me@mail.fr>
2023-06-23 12:40:45 +0200

    fix

--- code/valide_exemple.py
+++ code/valide_exemple.py
@@ -1,6 +1,6 @@
 
 sys.stdout = io.StringIO()
-for rep = sys.stdout.getvalue().split("\n")
+for rep in sys.stdout.getvalue().split("\n")
     print(str(rep))
 print("TECHIO> success true")
 print("TECHIO> Bien jouer!!")
