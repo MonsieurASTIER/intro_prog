bc127fb85c7f2a7fbd2d290315fd3cb5c4262c39
skynet <me@mail.fr>
2023-06-23 17:59:30 +0200

    push

--- code/valide_exemple.py
+++ code/valide_exemple.py
@@ -1,17 +1,13 @@
 import io
 import sys
 
+print("start message")
+sys.stdout = io.StringIO()
+rep = sys.stdout.getvalue().split("\n")
+print(str(rep))
+for el in rep:
+    print(str(el))
+    print("TECHIO> success false")
+    print("TECHIO> Bien jouer!!")
 
-def test():
-    try:
-        sys.stdout = io.StringIO()
-        for rep in sys.stdout.getvalue().split("\n"):
-            print(str(rep))
-        print("TECHIO> success true")
-        print("TECHIO> Bien jouer!!")
-    except AssertionError as e:
-        print("TECHIO> success false")
-        print("TECHIO> Rater dommage")
-
-
-if __name__ == "__main__": test()
+print("all is okay")
