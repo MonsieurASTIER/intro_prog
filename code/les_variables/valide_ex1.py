5084c89dfbbf2d775c527711c0f6e044b497e03c
skynet <me@mail.fr>
2023-07-11 15:09:19 +0200

    exception

--- code/les_variables/valide_ex1.py
+++ code/les_variables/valide_ex1.py
@@ -18,13 +18,14 @@ def test_Verif():
             if reponse != 22:
                 print("TECHIO> success false")
                 send_msg("Echecs","Ce n'est pas la sortie attendu")
-
+                assert False "echec"
         print(str(rep))
-
-
     except AssertionError as e:
         print("TECHIO> success false")
         send_msg("Echecs","recommence")
+    except Exception as e:
+        print("exception")
+        print(e)
 
 if __name__ == "__main__":
     print("main")
