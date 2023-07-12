34f502eb016c68fb6349de6faf63e75ac64c6841
skynet <me@mail.fr>
2023-07-12 15:33:13 +0200

    commit

--- code/les_variables/valide_ex1.py
+++ code/les_variables/valide_ex1.py
@@ -21,7 +21,7 @@ def test_Verif():
         print("inside the try")
         for i,reponse in enumerate(rep):
             print(str(i) + " : " + str(reponse))
-            if reponse != 22:
+            if reponse != "22":
                 print("TECHIO> success false")
                 send_msg("Echecs","Ce n'est pas la sortie attendu")
                 #assert False , ("echec ABCD")
